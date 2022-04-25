7030553fe0e92efd5accc6caee6d9c9748b29d25
oebars <oebars@gmail.com>
2022-04-25 22:16:44 +0200

    Refactor into nested routing  to prepare for multiple stocks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import './App.css';
-import MainContent from './content/MainContent';
 import './Navbar.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Sidebar from './navigation/Sidebar';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import About from './content/About';
+import { Outlet } from "react-router-dom";
 
 
 function App() {
   return (
-
-      <Router>
-        <div className="App">
-        <Sidebar />
-        <Routes>
-          <Route path="/" element={<MainContent />} />
-          <Route path="search" element={<MainContent />} />
-          <Route path="about" element={<About />} />
-        </Routes>
-        </div>
-      </Router>
-    
+    <div className="App">
+      <Sidebar />
+      <Outlet />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 import 'normalize.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MainContent from './content/MainContent';
+import About from './content/About';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="about" element={<About />} />
+          <Route path="search" element={<MainContent />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>,
   </React.StrictMode>
 );
 
