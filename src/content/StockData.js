6a58870202e889f5d6c1f50ecfd17e7766e1e42e
itractor <oebars@gmail.com>
2022-04-27 22:27:38 +0200

    WIP

--- src/content/StockData.js
+++ src/content/StockData.js
@@ -1,15 +1,59 @@
-import JsonData from './Test.json'
+import useState from "react";
+
+async function GetStockByTicker() {
+  fetch('http://localhost:8080/stock/MOWI').then(async (response) => {
+    const data = await response.json();
+    console.log("test" + data)
+    return data;
+  })
+}
+
 
 function StockData() {
-    const Data = JsonData.map(
+    const [getResult, setGetResult] = useState(null);
+    const fortmatResponse = (res) => {
+      return JSON.stringify(res, null, 2);
+    }
+
+
+    async function getDataById() {
+        
+        try {
+            const res = await fetch('http://localhost:8080/stock/MOWI');
+            if (!res.ok) {
+                const message = `An error has occured: ${res.status} - ${res.statusText}`;
+                throw new Error(message);
+            }
+            const data = await res.json();
+            const result = {
+                data: data,
+                status: res.status,
+                statusText: res.statusText,
+                headers: {
+                "Content-Type": res.headers.get("Content-Type"),
+                "Content-Length": res.headers.get("Content-Length"),
+                },
+            };
+            setGetResult(fortmatResponse(result));
+        } catch (err) {
+            setGetResult(err.message);
+        }
+    }
+      
+
+    const Data = getDataById().map(
         (info)=>{
+
+            console.log(info.reportyear);
+            console.log(setGetResult)
+
             return(
                 <tr>
                     <td>
-                        {info.year}
+                        {info.reportyear}
                     </td>
                     <td>
-                        {info.price}
+                        To be integrated
                     </td>
                     <td>
                         {info.totalEarnings}
